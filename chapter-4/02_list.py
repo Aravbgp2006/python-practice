81654b08a9dfceb4fc0ed6e3cb20ef1d1280ba5f
Arav Raj <arav111bgp11@gmail.com>
2026-06-23 10:31:31 +0530

    Add files via upload

---
+++ chapter-4/02_list.py
@@ -0,0 +1,14 @@
+# marks= []
+# f1= int(input("enter marks here:"))  
+# marks.append(f1)
+# f1= int(input("enter marks here:"))  
+# marks.append(f1)
+# f1= int(input("enter marks here:"))  
+# marks.append(f1)
+# f1= int(input("enter marks here:"))  
+# marks.append(f1)
+# f1= int(input("enter marks here:"))  
+# marks.append(f1)
+a= ( 34, 234, "harry")
+a[2]= "larry" 
+    
