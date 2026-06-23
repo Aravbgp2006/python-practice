cb027da62bc8b88535ac2317afa16de9808e6d1b
Arav Raj <arav111bgp11@gmail.com>
2026-06-23 10:28:21 +0530

    Add files via upload

---
+++ chapter-1/problem3.py
@@ -0,0 +1,8 @@
+# Program: Student Management System
+# Purpose: Add student details
+
+# Taking input from user
+name = input("Enter student name: ")
+
+# Displaying output
+print("Student Name:", name)
